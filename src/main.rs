b13ee7c5e8378982e48da1e4cf08e7b800d8be08
feras.albaroudi@gmail.com <feras.albaroudi@gmail.com>
2020-08-14 04:23:26 +1200

    Draw board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+const ROWS: usize = 15;
+const COLS: usize = 17;
+
+const HORIZONTAL_BORDER: char = '#';
+const VERTICAL_BORDER: char = '#';
+const EMPTY_CELL: char = ' ';
+
 fn main() {
     println!("Hello, world!");
+
+    let mut cells = [[0; COLS]; ROWS];
+
+    cells[0][0] = 5;
+    print_screen(cells);
+}
+
+fn print_horizontal_border() {
+    for _ in 0..(COLS + 2) {
+        print!("{}", HORIZONTAL_BORDER);
+    }
+    println!("");
+}
+
+fn print_screen(cells: [[i32; COLS]; ROWS]) {
+    print_horizontal_border();
+
+    for row in cells.iter() {
+        print!("{}", VERTICAL_BORDER);
+        for col in row.iter() {
+            let value = *col;
+            if value == 0 {
+                print!("{}", EMPTY_CELL);
+            } else {
+                print!("{}", value);
+            }
+        }
+        println!("{}", VERTICAL_BORDER);
+    }
+
+    print_horizontal_border();
 }
